ebf8545da680eb336fdb097fd17f7166c02a10bc
MaxDor <la2web@hotmail.com>
2021-12-08 18:50:06 +0300

    Пофиксил косяки , доделал второе занятие до конца

--- js/services.js
+++ js/services.js
@@ -1,5 +1,6 @@
 const API_KEY ='dbe49e48993a4ad8f07dfef65713470c';
 const BASE_URL= 'https://api.themoviedb.org/3/';
+const LANGUAGE = '&language=ru-RU';
 
 //trending/all/day?api_key=<<api_key>>//
 
@@ -10,11 +11,9 @@ const getData = url => fetch(url)
         }
         throw `Что - то пошло не так , ошибка ${response.status}`
 })
-
 .catch(err => console.error(err));
 
-export const getTriends = async ()=> {
-    const url = `${BASE_URL}trending/all/day?api_key=${API_KEY}`;
-    const data = await getData(url);
-    console.log('data:', data);
+export const getTriends = async (type = 'all',period = 'week',page = 1)=> {
+    const url = `${BASE_URL}trending/${type}/${period}?api_key=${API_KEY}${LANGUAGE}&page=${page}`;
+    return await getData(url);
 };
