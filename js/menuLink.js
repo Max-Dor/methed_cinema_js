18f8321ca6724e0d039eab09bf9ecc1537a14b09
MaxDor <la2web@hotmail.com>
2021-12-10 17:26:57 +0300

    Сделал домашку третьего дня . Добавил ВЕСЬ список меню . ВЕСЬ

--- js/menuLink.js
+++ js/menuLink.js
@@ -1,6 +1,7 @@
 import {
     getPopular,
-    getTop
+    getTop,
+    getTriends
 } from'./services.js';
 import renderCard from './renderCard.js';
 
@@ -19,6 +20,10 @@ const menuLink = () =>{
                 filmWeek.style.display = 'none';
                 title.textContent = target.textContent;
 
+                if(target.classList.contains('get-nav__link_triends')) {
+                    getTriends('movie')
+                    .then(data => renderCard(data.results))
+                }
                 if(target.classList.contains('get-nav__link_popular-movies')) {
                     getPopular('movie')
                     .then(data => renderCard(data.results))
